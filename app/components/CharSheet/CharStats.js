fba09e0c4ade015df47d438cfbbac5d4553a707a
Vincent Nguyen <vpvnguyen@gmail.com>
2020-03-07 12:11:44 -0800

    initial components within charactersheet

---
+++ app/components/CharSheet/CharStats.js
@@ -0,0 +1,48 @@
+// COLUMN 1: Str, Dex, Constitution, Intel, Wis, Charisma
+// COLUMN 2:
+// - ROW 1: Inspiration
+// - ROW 2: Proficiency Bonus
+// - ROW 3: (saving throws) str, dex, const, intel, wis, char
+// - ROW 4: (skills) Acrobatics(dex), Animal Handling(wis), Arcana(int), Athletics(str), deception(cha), history(int), insight(wis), intimidation(cha),
+// - ROW 5:
+
+// STATS
+// STRENGTH
+// DEXTERITY
+// CONSTITUTION
+// INTELLIGENCE
+// WISDOM
+// CHARISMA
+
+// INSPIRATION
+// PROFICIENCY BONUS
+
+// SKILLS
+// Acrobatics (Dex)
+// Animal Handling (Wis)
+// Arcana (Int)
+// Athletics (Str)
+// Deception (Cha)
+// History (Int)
+// Insight (Wis)
+// Intimidation (Cha)
+// Investigation (Int)
+// Medicine (Wis)
+// Nature (Int)
+// Perception (Wis)
+// Performance (Cha)
+// Persuasion (Cha)
+// Religion (Int)
+// Sleight of Hand (Dex)
+// Stealth (Dex)
+// Survival (Wis)
+
+// SAVING THROWS
+// Strength
+// Dexterity
+// Constitution
+// Intelligence
+// Wisdom
+// Charisma
+
+// PASSIVE WISDOM (PERCEPTION)
