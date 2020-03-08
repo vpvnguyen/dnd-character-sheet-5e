78aa14ca54aeeba0db5019bbf53aceb6646cd7b8
Vincent Nguyen <vpvnguyen@gmail.com>
2020-03-08 13:06:50 -0700

    pass character information to createChar

--- app/utils/charSheet.API.js
+++ app/utils/charSheet.API.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default {
   // create character
   createChar: input => {
-    return axios.post("http://localhost:3001/createChar/");
+    return axios.post("http://localhost:3001/createChar/", input);
   }
 };
