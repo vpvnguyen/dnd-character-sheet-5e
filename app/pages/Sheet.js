950f0d49c8bf62e6d1724ae2c553f0e5292afab8
Vincent Nguyen <vpvnguyen@gmail.com>
2020-03-13 16:32:16 -0700

    added layout component

--- app/pages/Sheet.js
+++ app/pages/Sheet.js
@@ -1,23 +1,12 @@
 import React from "react";
-import Head from "next/head";
+
+import Layout from "../components/Layout";
 import CharHeader from "../components/CharSheet/CharHeader";
 import CharStats from "../components/CharSheet/CharStats";
 
 const Sheet = () => (
   <>
-    <Head>
-      <title>Sheet</title>
-      <link rel="icon" href="/favicon.ico" />
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-      />
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/icon?family=Material+Icons"
-      />
-    </Head>
-
+    <Layout />
     <CharHeader />
     <CharStats />
   </>
