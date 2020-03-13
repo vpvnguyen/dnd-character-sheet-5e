b86a57ef7a64a6d9d0c09ac193e391e1f1ec76e6
Vincent Nguyen <vpvnguyen@gmail.com>
2020-03-13 16:32:52 -0700

    removed Head next-component; added layout component

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,23 +1,19 @@
-import React from 'react'
-import Head from 'next/head'
-import Nav from '../components/nav'
-import Login from '../components/Login'
-import SignIn from './SignIn';
+import React from "react";
+
+import Layout from "../components/Layout";
+import Nav from "../components/nav";
+import Login from "../components/Login";
+import SignIn from "./SignIn";
 
 const Home = () => (
   <>
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-    </Head>
+    <Layout />
 
     {/* <Nav /> */}
-  
-  {/* <Login /> */}
-  <SignIn />
-</>
-)
 
-export default Home
+    {/* <Login /> */}
+    <SignIn />
+  </>
+);
+
+export default Home;
