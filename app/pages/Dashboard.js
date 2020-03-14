01046ddd4be61eb06eb98e51c689578b097415b6
Vincent Nguyen <vpvnguyen@gmail.com>
2020-03-13 23:10:48 -0700

    use batman api for dummy data

--- app/pages/Dashboard.js
+++ app/pages/Dashboard.js
@@ -7,15 +7,15 @@ import fetch from "isomorphic-unfetch";
 const Dashboard = props => (
   <Layout>
     <div>User's dashboard</div>
-    {/* <ul>
-        {props.shows.map(show => (
-          <li key={show.id}>
-            <Link href="/p/[id]" as={`/p/${show.id}`}>
-              <a>{show.name}</a>
-            </Link>
-          </li>
-        ))}
-      </ul> */}
+    <ul>
+      {props.shows.map(show => (
+        <li key={show.id}>
+          <Link href="/p/[id]" as={`/p/${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
     <div>
       {/* {props.users.map(user => (
         <li key={user.id}>
@@ -34,15 +34,15 @@ const Dashboard = props => (
 );
 
 // fetch data from charSheets API
-Dashboard.getInitialProps = async () => {
+Dashboard.getInitialProps = async function() {
   const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
 
   //   const res = await fetch("http://localhost:3001/getChar");
-  const data = await res.json;
+  const data = await res.json();
 
   console.log(`Show data fetched. Count: ${data.length}`);
 
-  return { shows: "show" };
+  return { shows: data.map(entry => entry.show) };
 };
 
 export default Dashboard;
