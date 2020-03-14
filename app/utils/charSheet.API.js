520838baba731f194661694e0ed4c54637f99ba6
Vincent Nguyen <vpvnguyen@gmail.com>
2020-03-13 23:11:33 -0700

    test GET and POST character sheet routes

--- app/utils/charSheet.API.js
+++ app/utils/charSheet.API.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 export default {
   // create character
+  getChar: () => {
+    return axios.get("http://localhost:3001/getChar/");
+  },
   createChar: input => {
     return axios.post("http://localhost:3001/createChar/", input);
   }
