19ac7215c676a40a8d137f46437ce67b02b234f8
Vincent Nguyen <vpvnguyen@gmail.com>
2020-03-14 03:06:09 -0700

    removed material-table

--- app/pages/Sheet.js
+++ app/pages/Sheet.js
@@ -4,6 +4,7 @@ import Layout from "../components/Layout";
 import CharHeader from "../components/CharSheet/CharHeader";
 import CharStats from "../components/CharSheet/CharStats";
 import Panel from "../test/Panel";
+
 const Sheet = () => (
   <>
     <Layout>
