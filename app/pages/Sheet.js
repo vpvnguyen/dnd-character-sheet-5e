0ff6820d2190cb66a7e855b60f8b64cd0bea6eba
Vincent Nguyen <vpvnguyen@gmail.com>
2020-03-14 02:42:23 -0700

    import expansion panels to segregate character sheet components

--- app/pages/Sheet.js
+++ app/pages/Sheet.js
@@ -3,12 +3,13 @@ import React from "react";
 import Layout from "../components/Layout";
 import CharHeader from "../components/CharSheet/CharHeader";
 import CharStats from "../components/CharSheet/CharStats";
-
+import Panel from "../test/Panel";
 const Sheet = () => (
   <>
     <Layout>
       <CharHeader />
       <CharStats />
+      <Panel />
     </Layout>
   </>
 );
