2b2fa2350f0beec84cb1307f3c3e4687c04a70c4
Vincent Nguyen <vpvnguyen@gmail.com>
2020-03-13 23:09:51 -0700

    fetch and display dummy api data

--- app/pages/Dashboard.js
+++ app/pages/Dashboard.js
@@ -2,14 +2,47 @@ import React from "react";
 import Link from "next/link";
 import Layout from "../components/Layout";
 
-export default function Dashboard() {
-  return (
-    <>
-      <Layout />
-      <div>User's dashboard</div>
+import fetch from "isomorphic-unfetch";
+
+const Dashboard = props => (
+  <Layout>
+    <div>User's dashboard</div>
+    {/* <ul>
+        {props.shows.map(show => (
+          <li key={show.id}>
+            <Link href="/p/[id]" as={`/p/${show.id}`}>
+              <a>{show.name}</a>
+            </Link>
+          </li>
+        ))}
+      </ul> */}
+    <div>
+      {/* {props.users.map(user => (
+        <li key={user.id}>
+          <Link href="/p/[id]" as={`/p/${user.id}`}>
+            <a>Username: {user.name}</a>
+          </Link>
+        </li>
+      ))} */}
+    </div>
+    <div>
       <Link href="/Sheet">
         <button>character sheet</button>
       </Link>
-    </>
-  );
-}
+    </div>
+  </Layout>
+);
+
+// fetch data from charSheets API
+Dashboard.getInitialProps = async () => {
+  const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
+
+  //   const res = await fetch("http://localhost:3001/getChar");
+  const data = await res.json;
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return { shows: "show" };
+};
+
+export default Dashboard;
