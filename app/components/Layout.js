d82c73c8cd76db5fcb02f2e68e64d5ca84290a92
Vincent Nguyen <vpvnguyen@gmail.com>
2020-03-13 23:03:37 -0700

    wip: adding data detch

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -1,20 +1,25 @@
-import { CssBaseline } from "@material-ui/core";
+import { CssBaseline, Grid } from "@material-ui/core";
 import Header from "./Header";
 
 const layoutStyle = {
   //   margin: "auto",
   //   padding: 20,
   //   border: "1px solid #DDD"
+  display: "flex",
+  flexDirection: "column",
+  textAlign: "center"
+  //   flexWrap: "wrap",
 };
 
 const Layout = props => (
   <div style={layoutStyle}>
     <CssBaseline />
     <Header />
-
-    {/* If you remove {props.children}, the Layout cannot render the content inside the Layout element wrapped in other components. */}
-    {/* {props.children} */}
-    {props.content}
+    <Grid item xs>
+      {/* If you remove {props.children}, the Layout cannot render the content inside the Layout element wrapped in other components. */}
+      {props.children}
+      {props.content}
+    </Grid>
   </div>
 );
 
