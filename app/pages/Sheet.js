ae1a1974cc4ccad986e71277a35c178e9bc9a145
vpvnguyen <vpvnguyen@gmail.com>
2020-03-17 22:37:48 -0700

    disable test components

--- app/pages/Sheet.js
+++ app/pages/Sheet.js
@@ -3,14 +3,14 @@ import React from "react";
 import Layout from "../components/Layout";
 import CharHeader from "../components/CharSheet/CharHeader";
 import CharStats from "../components/CharSheet/CharStats";
-import Panel from "../test/Panel";
+// import Panel from "../test/Panel";
 
 const Sheet = () => (
   <>
     <Layout>
       <CharHeader />
       <CharStats />
-      <Panel />
+      {/* <Panel /> */}
     </Layout>
   </>
 );
