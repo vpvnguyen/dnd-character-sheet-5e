1e42642e3bdf8107602715c746b58041d6722bf2
Vincent Nguyen <vpvnguyen@gmail.com>
2020-03-20 23:57:42 -0700

    add HitDice component; getInitialProps from CharHP API placeholder

--- app/components/CharSheet/CharHP.js
+++ app/components/CharSheet/CharHP.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { Card } from "@material-ui/core";
-// ARMOR CLASS INITIATIVE SPEED
+import fetch from "isomorphic-unfetch";
 
+// ARMOR CLASS INITIATIVE SPEED
 const ArmorClass = () => {
   const [armor, setArmor] = useState(0);
   return (
@@ -10,9 +11,9 @@ const ArmorClass = () => {
     </div>
   );
 };
+
 // CURRENT HIT POINTS / MAX
 // TEMPORARY HIT POINTS
-
 const HP = () => {
   const [hp, setHp] = useState(0);
   const [tempHp, setTempHp] = useState(0);
@@ -23,7 +24,11 @@ const HP = () => {
     </div>
   );
 };
+
 // Total / HIT DICE
+const HitDice = () => {
+  return <div>HIT DICE</div>;
+};
 
 // DEATH SAVES
 // 3x success
@@ -55,11 +60,31 @@ const CharHP = () => {
       <div>CharHP</div>
       <HP />
       <ArmorClass />
+      <HitDice />
       <DeathSaves />
     </Card>
   );
 };
 
-// fetch character armor class, hp, temp hp, hit dice from saved sheet
+// fetch character armor class, hp, temp hp, hit dice from saved sheet API
+
+CharHP.getInitialProps = async () => {
+  // const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
+
+  //   const res = await fetch("http://localhost:3001/getChar");
+  // const data = await res.json();
+  console.log(`CharHP data fetched. Count: ${data.length}`);
+
+  // const getCharData = await charSheetAPI
+  //   .getChar()
+  //   .catch(err => console.log(err))
+  //   .then(logging => logging.data.getChar);
+  // .then(result => result.data);
+
+  const string = "CharHP API";
+  // return { shows: data.map(entry => entry.show), string, getCharData };
+  // return { shows: data.map(entry => entry.show), string };
+  return string;
+};
 
 export default CharHP;
