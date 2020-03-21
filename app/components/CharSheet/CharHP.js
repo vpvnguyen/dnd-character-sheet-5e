0aa2ecc49331baab7c39e0cd5539b291d93180b0
Vincent Nguyen <vpvnguyen@gmail.com>
2020-03-20 23:47:15 -0700

    created DeathSaves component and state

--- app/components/CharSheet/CharHP.js
+++ app/components/CharSheet/CharHP.js
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+import { Card } from "@material-ui/core";
 // ARMOR CLASS INITIATIVE SPEED
 
 // CURRENT HIT POINTS / MAX
@@ -8,12 +10,37 @@
 // DEATH SAVES
 // 3x success
 // 3x failure
+// render death saves component
+const DeathSaves = () => {
+  const [deathSaves, setDeathSaves] = useState([
+    { success: "success", qty: 3 },
+    { fail: "fail", qty: 3 }
+  ]);
 
-import React from "react";
-import { Card } from "@material-ui/core";
+  console.log(deathSaves);
+  return (
+    <div>
+      Death Saves:
+      <div>
+        {deathSaves[0].success}: {deathSaves[0].qty}
+      </div>
+      <div>
+        {deathSaves[1].fail}: {deathSaves[1].qty}
+      </div>
+    </div>
+  );
+};
 
 const CharHP = () => {
-  return <Card>CharHP</Card>;
+  const [hp, setHp] = useState("");
+  return (
+    <Card>
+      <div>CharHP</div>
+      <DeathSaves />
+    </Card>
+  );
 };
+
+// fetch character armor class, hp, temp hp, hit dice from saved sheet
 
 export default CharHP;
