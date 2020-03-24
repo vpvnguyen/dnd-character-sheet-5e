643ceb3e44a5d05c685f27deb466002d1c1fb79a
Vincent Nguyen <vpvnguyen@gmail.com>
2020-03-23 20:01:32 -0700

    add ideals, bonds, and flaws note sections

--- app/components/CharSheet/CharTraits.js
+++ app/components/CharSheet/CharTraits.js
@@ -5,15 +5,45 @@
 // BONDS
 // FLAWS
 import React from "react";
-import { Card } from "@material-ui/core";
+import { Card, Grid, TextField } from "@material-ui/core";
 
 const CharTraits = () => {
   return (
     <Card>
       <h3>CharTraits</h3>
-      <div>Ideals notes</div>
-      <div>Bonds notes</div>
-      <div>flaws notes</div>
+      <Grid>
+        <TextField
+          id="outlined-multiline-static"
+          label="IDEALS notes section"
+          multiline
+          rows="2"
+          variant="outlined"
+          // value={charDesc}
+          // onChange={handleCharDescChange}
+        />
+      </Grid>
+      <Grid>
+        <TextField
+          id="outlined-multiline-static"
+          label="BONDS notes section"
+          multiline
+          rows="2"
+          variant="outlined"
+          // value={charDesc}
+          // onChange={handleCharDescChange}
+        />
+      </Grid>
+      <Grid>
+        <TextField
+          id="outlined-multiline-static"
+          label="FLAWS notes section"
+          multiline
+          rows="2"
+          variant="outlined"
+          // value={charDesc}
+          // onChange={handleCharDescChange}
+        />
+      </Grid>
     </Card>
   );
 };
