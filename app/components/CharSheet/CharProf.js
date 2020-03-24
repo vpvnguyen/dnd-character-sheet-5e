647b7effd4dc9d9354d89016b9505a43089eb387
Vincent Nguyen <vpvnguyen@gmail.com>
2020-03-23 19:59:43 -0700

    add notes field

--- app/components/CharSheet/CharProf.js
+++ app/components/CharSheet/CharProf.js
@@ -1,13 +1,23 @@
 // OTHER PROFICIENCIES & LANGUAGES
 // NOTES
 import React from "react";
-import { Card } from "@material-ui/core";
+import { Card, Grid, TextField } from "@material-ui/core";
 
 const CharProf = () => {
   return (
     <Card>
       <h3>CharProf</h3>
-      <div>NOTES SECTION</div>
+      <Grid>
+        <TextField
+          id="outlined-multiline-static"
+          label="Description"
+          multiline
+          rows="2"
+          variant="outlined"
+          // value={charDesc}
+          // onChange={handleCharDescChange}
+        />
+      </Grid>
     </Card>
   );
 };
