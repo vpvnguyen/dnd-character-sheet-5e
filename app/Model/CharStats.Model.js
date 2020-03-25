44879ef24a261116bff4a71eb9e6645c7d79d526
Vincent Nguyen <vpvnguyen@gmail.com>
2020-03-24 23:06:07 -0700

    created CharStatsModel for skills and stats

---
+++ app/Model/CharStats.Model.js
@@ -0,0 +1,86 @@
+const CharStatsModel = {
+  stats: [
+    "Strength",
+    "Dexterity",
+    "Constitution",
+    "Intelligence",
+    "Wisdom",
+    "Charisma"
+  ],
+  skills: [
+    {
+      name: "Acrobatics",
+      type: "Dex"
+    },
+    {
+      name: "Animal Handling",
+      type: "Wis"
+    },
+    {
+      name: "Arcana",
+      type: "Int"
+    },
+    {
+      name: "Athletics",
+      type: "Str"
+    },
+    {
+      name: "Deception",
+      type: "Cha"
+    },
+    {
+      name: "History",
+      type: "Int"
+    },
+    {
+      name: "Insight",
+      type: "Wis"
+    },
+    {
+      name: "Intimidation",
+      type: "Cha"
+    },
+    {
+      name: "Investigation",
+      type: "Int"
+    },
+    {
+      name: "Medicine",
+      type: "Wis"
+    },
+    {
+      name: "Nature",
+      type: "Int"
+    },
+    {
+      name: "Perception",
+      type: "Wis"
+    },
+    {
+      name: "Performance",
+      type: "Cha"
+    },
+    {
+      name: "Persuasion",
+      type: "Cha"
+    },
+    {
+      name: "Religion",
+      type: "Int"
+    },
+    {
+      name: "Sleight of Hand",
+      type: "Dex"
+    },
+    {
+      name: "Stealth",
+      type: "Dex"
+    },
+    {
+      name: "Survival",
+      type: "Wis"
+    }
+  ]
+};
+
+export default CharStatsModel;
