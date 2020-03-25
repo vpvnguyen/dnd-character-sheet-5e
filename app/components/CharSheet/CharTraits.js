162c9c73f8f7b8d79cebdb14b4b6596fdaa975f4
Vincent Nguyen <vpvnguyen@gmail.com>
2020-03-24 22:01:04 -0700

    refactored to dynamically render each trait

--- app/components/CharSheet/CharTraits.js
+++ app/components/CharSheet/CharTraits.js
@@ -1,49 +1,25 @@
-// ALL NOTES
-
-// PERSONALITY TRAITS
-// IDEALS
-// BONDS
-// FLAWS
 import React from "react";
 import { Card, Grid, TextField } from "@material-ui/core";
 
 const CharTraits = () => {
+  const traits = ["Personality Traits", "Ideals", "Bonds", "Flaws"];
+
   return (
     <Card>
       <h3>CharTraits</h3>
-      <Grid>
-        <TextField
-          id="outlined-multiline-static"
-          label="IDEALS notes section"
-          multiline
-          rows="2"
-          variant="outlined"
-          // value={charDesc}
-          // onChange={handleCharDescChange}
-        />
-      </Grid>
-      <Grid>
-        <TextField
-          id="outlined-multiline-static"
-          label="BONDS notes section"
-          multiline
-          rows="2"
-          variant="outlined"
-          // value={charDesc}
-          // onChange={handleCharDescChange}
-        />
-      </Grid>
-      <Grid>
-        <TextField
-          id="outlined-multiline-static"
-          label="FLAWS notes section"
-          multiline
-          rows="2"
-          variant="outlined"
-          // value={charDesc}
-          // onChange={handleCharDescChange}
-        />
-      </Grid>
+      {traits.map((value, index) => (
+        <Grid key={index}>
+          <TextField
+            id="outlined-multiline-static"
+            label={value}
+            multiline
+            rows="2"
+            variant="outlined"
+            placeholder={value}
+            // onChange={handleCharDescChange}
+          />
+        </Grid>
+      ))}
     </Card>
   );
 };
