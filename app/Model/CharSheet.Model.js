5479c2fe6780e826078815317908757989771600
Vincent Nguyen <vpvnguyen@gmail.com>
2020-03-26 23:36:44 -0700

    create CharSheetModel

---
+++ app/Model/CharSheet.Model.js
@@ -0,0 +1,66 @@
+const CharSheetModel = {
+  stats: {
+    str: "Strength",
+    dex: "Dexterity",
+    con: "Constitution",
+    int: "Intelligence",
+    wis: "Wisdom",
+    cha: "Charisma"
+  },
+  skills: {
+    acrobatics: "Acrobatics",
+    // type: "Dex"
+    animalHandling: "Animal Handling",
+    // type: "Wis"
+    arcana: "Arcana",
+    // type: "Int"
+
+    athletics: "Athletics",
+    // type: "Str"
+
+    deception: "Deception",
+    // type: "Cha"
+
+    history: "History",
+    // type: "Int"
+
+    insight: "Insight",
+    // type: "Wis"
+
+    intimidation: "Intimidation",
+    // type: "Cha"
+
+    investigation: "Investigation",
+    // type: "Int"
+
+    medicine: "Medicine",
+    // type: "Wis"
+
+    nature: "Nature",
+    // type: "Int"
+
+    perception: "Perception",
+    // type: "Wis"
+
+    performance: "Performance",
+    // type: "Cha"
+
+    persuasion: "Persuasion",
+    // type: "Cha"
+
+    Religion: "Religion",
+    // type: "Int"
+
+    sleightOfHand: "Sleight of Hand",
+    // type: "Dex"
+
+    stealth: "Stealth",
+    // type: "Dex"
+
+    survival: "Survival"
+    // type: "Wis"
+  },
+  statsSubStr: subStr => subStr.substring(0, 3)
+};
+
+console.log(CharSheetModel.statsSubStr(CharSheetModel.stats.str)); // Str
