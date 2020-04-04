53d676416a875b7065ebaa46555a27229346fc9d
Vincent Nguyen <vpvnguyen@gmail.com>
2020-04-04 12:45:55 -0700

    replaced attributes and skills with number fields

--- app/components/CharSheet/CharStats.js
+++ app/components/CharSheet/CharStats.js
@@ -6,26 +6,28 @@ import {
   Divider,
   InputLabel,
   Input,
-  FormControl
+  FormControl,
+  Typography,
+  TextField,
 } from "@material-ui/core";
 
 import CharStatsModel from "../../Model/CharStats.Model";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     overflow: "hidden",
-    padding: theme.spacing(0, 0)
+    padding: theme.spacing(0, 0),
   },
   paper: {
     // maxWidth: 400, // card width
     margin: `${theme.spacing(1)}px auto`, // space veritcally between cards
     textAlign: "center",
-    padding: theme.spacing(2) // veritcal padding between cards
+    padding: theme.spacing(2), // veritcal padding between cards
   },
   divider: {
-    margin: "15px"
-  }
+    margin: "15px",
+  },
 }));
 
 const CharStats = () => {
@@ -33,36 +35,63 @@ const CharStats = () => {
   return (
     <div className={classes.root}>
       <Paper className={classes.paper}>
+        <Typography variant="subtitle1" gutterBottom>
+          Attributes
+        </Typography>
+        <Divider className={classes.divider} />
         <Grid container spacing={3}>
-          {CharStatsModel.stats.map((value, index) => (
-            <div>
+          <FormControl>
+            {CharStatsModel.stats.map((value, index) => (
               <Grid item xs key={index}>
-                <FormControl>
-                  <InputLabel>{value}</InputLabel>
-                  <Input fullWidth placeholder={value} />
-                </FormControl>
+                <TextField
+                  id={`attribute-${value}`}
+                  label={value}
+                  type="number"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  variant="outlined"
+                />
               </Grid>
-            </div>
-          ))}
+            ))}
+          </FormControl>
         </Grid>
+
+        <Divider className={classes.divider} />
+        <div>SKILLS</div>
+        {CharStatsModel.skills.map((value, index) => (
+          <Grid item xs key={index}>
+            <TextField
+              id={`attribute-${value.name}`}
+              label={`${value.name} (${value.type})`}
+              type="number"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+            />
+          </Grid>
+        ))}
+        <Divider className={classes.divider} />
+        <div>SAVING THROWS</div>
+        {CharStatsModel.stats.map((value, index) => (
+          <Grid item xs key={index}>
+            <TextField
+              id={`attribute-${value}`}
+              label={value}
+              type="number"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+            />
+          </Grid>
+        ))}
 
         <Divider className={classes.divider} />
 
         <div>INSPIRATION</div>
         <div>PROFICIENCY BONUS</div>
-
-        <Divider className={classes.divider} />
-        <div>SKILLS</div>
-        {CharStatsModel.skills.map((value, index) => (
-          <div>
-            {value.name} ({value.type})
-          </div>
-        ))}
-        <Divider className={classes.divider} />
-        <div>SAVING THROWS</div>
-        {CharStatsModel.stats.map((value, index) => (
-          <div>{value}</div>
-        ))}
         <Divider className={classes.divider} />
 
         <div>PASSIVE WISDOM (PERCEPTION)</div>
