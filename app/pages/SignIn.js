5044c2f4883936df059c47ace0db3181a06dade0
Vincent Nguyen <vpvnguyen@gmail.com>
2020-04-05 15:34:26 -0700

    removed layout

--- app/pages/SignIn.js
+++ app/pages/SignIn.js
@@ -12,25 +12,25 @@ import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-
-const useStyles = makeStyles(theme => ({
+import { CssBaseline } from "@material-ui/core";
+const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
     display: "flex",
     flexDirection: "column",
-    alignItems: "center"
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main
+    backgroundColor: theme.palette.secondary.main,
   },
   form: {
     width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1)
+    marginTop: theme.spacing(1),
   },
   submit: {
-    margin: theme.spacing(3, 0, 2)
-  }
+    margin: theme.spacing(3, 0, 2),
+  },
 }));
 
 const SignIn = () => {
@@ -38,6 +38,7 @@ const SignIn = () => {
 
   return (
     <>
+      <CssBaseline />
       <Header />
       <Container component="main" maxWidth="xs">
         <div className={classes.paper}>
@@ -75,6 +76,8 @@ const SignIn = () => {
               control={<Checkbox value="remember" color="primary" />}
               label="Remember me"
             />
+
+            {/* Buttons */}
             <Button
               type="submit"
               fullWidth
@@ -85,6 +88,17 @@ const SignIn = () => {
             >
               Sign In
             </Button>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              href="/SheetMockLayout" //  REPLACE WITH AUTH
+            >
+              Layout
+            </Button>
+
             <Grid container>
               <Grid item xs>
                 <Link href="#" variant="body2">
