5b92df137853fea66a1b07b4b2a5e010ba2d8f15
Vincent Nguyen <vpvnguyen@gmail.com>
2020-04-13 17:01:27 -0700

    removed paper component wrapper

--- app/components/CharSheet/CharHeader.js
+++ app/components/CharSheet/CharHeader.js
@@ -9,77 +9,77 @@ import {
   Input,
   FormControl,
   TextField,
-  MenuItem
+  MenuItem,
 } from "@material-ui/core";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     overflow: "hidden",
-    padding: theme.spacing(0, 0)
+    padding: theme.spacing(0, 0),
   },
   paper: {
     margin: `${theme.spacing(1)}px auto`, // drop shadow
     textAlign: "center",
-    padding: theme.spacing(2) // veritcal padding between cards
+    padding: theme.spacing(2), // veritcal padding between cards
   },
   divider: {
-    margin: "15px"
-  }
+    margin: "15px",
+  },
 }));
 
 const charSexes = [
   {
     value: "Male",
-    label: "Male"
+    label: "Male",
   },
   {
     value: "Female",
-    label: "Female"
+    label: "Female",
   },
   {
     value: "Neutral",
-    label: "Neutral"
-  }
+    label: "Neutral",
+  },
 ];
 
 const charAlignments = [
   {
     value: "LawfulGood",
-    label: "Lawful Good"
+    label: "Lawful Good",
   },
   {
     value: "NeutralGood",
-    label: "Neutral Good"
+    label: "Neutral Good",
   },
   {
     value: "ChaoticGood",
-    label: "Chaotic Good"
+    label: "Chaotic Good",
   },
   {
     value: "LawfulNeutral",
-    label: "Lawful Neutral"
+    label: "Lawful Neutral",
   },
   {
     value: "Neutral",
-    label: "Neutral"
+    label: "Neutral",
   },
   {
     value: "ChaoticNeutral",
-    label: "Chaotic Neutral"
+    label: "Chaotic Neutral",
   },
   {
     value: "LawfulEvil",
-    label: "Lawful Evil"
+    label: "Lawful Evil",
   },
   {
     value: "NeutralEvil",
-    label: "Neutral Evil"
+    label: "Neutral Evil",
   },
   {
     value: "ChaoticEvil",
-    label: "Chaotic Evil"
-  }
+    label: "Chaotic Evil",
+  },
 ];
 
 const CharHeader = () => {
@@ -92,151 +92,149 @@ const CharHeader = () => {
   const [charSex, setCharSex] = useState("");
   const [charDesc, setCharDesc] = useState("");
 
-  const handleCharNameChange = event => {
+  const handleCharNameChange = (event) => {
     setCharName(event.target.value);
     console.log(event.target.value);
   };
 
-  const handleCharClassChange = event => {
+  const handleCharClassChange = (event) => {
     setCharClass(event.target.value);
     console.log(event.target.value);
   };
 
-  const handleCharLevelChange = event => {
+  const handleCharLevelChange = (event) => {
     setCharLevel(event.target.value);
     console.log(event.target.value);
   };
 
-  const handleCharAlignmentChange = event => {
+  const handleCharAlignmentChange = (event) => {
     setCharAlignment(event.target.value);
     console.log(event.target.value);
   };
 
-  const handleCharRaceChange = event => {
+  const handleCharRaceChange = (event) => {
     setCharRace(event.target.value);
     console.log(event.target.value);
   };
 
-  const handleCharSexChange = event => {
+  const handleCharSexChange = (event) => {
     setCharSex(event.target.value);
     console.log(event.target.value);
   };
 
-  const handleCharDescChange = event => {
+  const handleCharDescChange = (event) => {
     setCharDesc(event.target.value);
     console.log(event.target.value);
   };
 
   return (
     <div className={classes.root}>
-      <Paper className={classes.paper}>
-        <Typography variant="subtitle1" gutterBottom>
-          Character Sheet 5e
-        </Typography>
+      <Typography variant="subtitle1" gutterBottom>
+        Character Sheet 5e
+      </Typography>
 
-        <Divider className={classes.divider} />
+      <Divider className={classes.divider} />
 
-        <Grid container spacing={3}>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <FormControl>
+            <InputLabel>Name</InputLabel>
+            <Input
+              label="myName"
+              fullWidth
+              placeholder="Names"
+              value={charName}
+              onChange={handleCharNameChange}
+            />
+          </FormControl>
+        </Grid>
+
+        <Grid item xs>
           <Grid item xs>
             <FormControl>
-              <InputLabel>Name</InputLabel>
-              <Input
-                label="myName"
-                fullWidth
-                placeholder="Names"
-                value={charName}
-                onChange={handleCharNameChange}
-              />
-            </FormControl>
-          </Grid>
-
-          <Grid item xs>
-            <Grid item xs>
-              <FormControl>
-                <InputLabel>Class</InputLabel>
-                <Input
-                  fullWidth
-                  placeholder="Class"
-                  value={charClass}
-                  onChange={handleCharClassChange}
-                />
-              </FormControl>
-            </Grid>
-          </Grid>
-
-          <Grid item xs>
-            <FormControl>
-              <InputLabel>Level</InputLabel>
+              <InputLabel>Class</InputLabel>
               <Input
                 fullWidth
-                placeholder="Level"
-                value={charLevel}
-                onChange={handleCharLevelChange}
+                placeholder="Class"
+                value={charClass}
+                onChange={handleCharClassChange}
               />
             </FormControl>
           </Grid>
         </Grid>
 
-        <Grid container spacing={3}>
-          <Grid item xs>
-            <TextField
-              id="char-alignment"
-              select
-              label="Alignment"
-              value={charAlignment}
-              onChange={handleCharAlignmentChange}
-              helperText="Select Alignment"
-            >
-              {charAlignments.map((val, index) => (
-                <MenuItem key={`char-alignment-${index}`} value={val.value}>
-                  {val.label}
-                </MenuItem>
-              ))}
-            </TextField>
-          </Grid>
-
-          <Grid item xs>
-            <FormControl>
-              <InputLabel>Race</InputLabel>
-              <Input
-                fullWidth
-                placeholder="Race"
-                value={charRace}
-                onChange={handleCharRaceChange}
-              />
-            </FormControl>
-          </Grid>
-
-          <Grid item xs>
-            <TextField
-              id="char-sex"
-              select
-              label="Sex"
-              value={charSex}
-              onChange={handleCharSexChange}
-              helperText="Select character sex"
-            >
-              {charSexes.map((val, index) => (
-                <MenuItem key={`char-sex-${index}`} value={val.value}>
-                  {val.label}
-                </MenuItem>
-              ))}
-            </TextField>
-          </Grid>
+        <Grid item xs>
+          <FormControl>
+            <InputLabel>Level</InputLabel>
+            <Input
+              fullWidth
+              placeholder="Level"
+              value={charLevel}
+              onChange={handleCharLevelChange}
+            />
+          </FormControl>
         </Grid>
+      </Grid>
 
-        <Grid>
+      <Grid container spacing={3}>
+        <Grid item xs>
           <TextField
-            id="outlined-multiline-static"
-            label="Description"
-            multiline
-            rows="2"
-            variant="outlined"
-            value={charDesc}
-            onChange={handleCharDescChange}
-          />
+            id="char-alignment"
+            select
+            label="Alignment"
+            value={charAlignment}
+            onChange={handleCharAlignmentChange}
+            helperText="Select Alignment"
+          >
+            {charAlignments.map((val, index) => (
+              <MenuItem key={`char-alignment-${index}`} value={val.value}>
+                {val.label}
+              </MenuItem>
+            ))}
+          </TextField>
         </Grid>
-      </Paper>
+
+        <Grid item xs>
+          <FormControl>
+            <InputLabel>Race</InputLabel>
+            <Input
+              fullWidth
+              placeholder="Race"
+              value={charRace}
+              onChange={handleCharRaceChange}
+            />
+          </FormControl>
+        </Grid>
+
+        <Grid item xs>
+          <TextField
+            id="char-sex"
+            select
+            label="Sex"
+            value={charSex}
+            onChange={handleCharSexChange}
+            helperText="Select character sex"
+          >
+            {charSexes.map((val, index) => (
+              <MenuItem key={`char-sex-${index}`} value={val.value}>
+                {val.label}
+              </MenuItem>
+            ))}
+          </TextField>
+        </Grid>
+      </Grid>
+
+      <Grid>
+        <TextField
+          id="outlined-multiline-static"
+          label="Description"
+          multiline
+          rows="2"
+          variant="outlined"
+          value={charDesc}
+          onChange={handleCharDescChange}
+        />
+      </Grid>
     </div>
   );
 };
