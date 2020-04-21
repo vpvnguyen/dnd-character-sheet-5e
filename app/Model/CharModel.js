4e1e026487722fd57986d04e18538ab5db3e6910
Vincent Nguyen <vpvnguyen@gmail.com>
2020-04-20 23:18:20 -0700

    cloned ChaSheet model; added modifiers to stats

---
+++ app/Model/CharModel.js
@@ -0,0 +1,147 @@
+const CharStatsModel = {
+  sex: [
+    {
+      value: "Male",
+      label: "Male",
+    },
+    {
+      value: "Female",
+      label: "Female",
+    },
+    {
+      value: "Neutral",
+      label: "Neutral",
+    },
+  ],
+  alignment: [
+    {
+      value: "LawfulGood",
+      label: "Lawful Good",
+    },
+    {
+      value: "NeutralGood",
+      label: "Neutral Good",
+    },
+    {
+      value: "ChaoticGood",
+      label: "Chaotic Good",
+    },
+    {
+      value: "LawfulNeutral",
+      label: "Lawful Neutral",
+    },
+    {
+      value: "Neutral",
+      label: "Neutral",
+    },
+    {
+      value: "ChaoticNeutral",
+      label: "Chaotic Neutral",
+    },
+    {
+      value: "LawfulEvil",
+      label: "Lawful Evil",
+    },
+    {
+      value: "NeutralEvil",
+      label: "Neutral Evil",
+    },
+    {
+      value: "ChaoticEvil",
+      label: "Chaotic Evil",
+    },
+  ],
+  stats: [
+    { name: "Strength", modifier: 0 },
+    { name: "Dexterity", modifier: 0 },
+    { name: "Constitution", modifier: 0 },
+    { name: "Constitution", modifier: 0 },
+    { name: "Intelligence", modifier: 0 },
+    { name: "Wisdom", modifier: 0 },
+    { name: "Charisma", modifier: 0 },
+  ],
+  stats2: [
+    "Strength",
+    "Dexterity",
+    "Constitution",
+    "Intelligence",
+    "Wisdom",
+    "Charisma",
+  ],
+  skills: [
+    {
+      name: "Acrobatics",
+      type: "Dex",
+    },
+    {
+      name: "Animal Handling",
+      type: "Wis",
+    },
+    {
+      name: "Arcana",
+      type: "Int",
+    },
+    {
+      name: "Athletics",
+      type: "Str",
+    },
+    {
+      name: "Deception",
+      type: "Cha",
+    },
+    {
+      name: "History",
+      type: "Int",
+    },
+    {
+      name: "Insight",
+      type: "Wis",
+    },
+    {
+      name: "Intimidation",
+      type: "Cha",
+    },
+    {
+      name: "Investigation",
+      type: "Int",
+    },
+    {
+      name: "Medicine",
+      type: "Wis",
+    },
+    {
+      name: "Nature",
+      type: "Int",
+    },
+    {
+      name: "Perception",
+      type: "Wis",
+    },
+    {
+      name: "Performance",
+      type: "Cha",
+    },
+    {
+      name: "Persuasion",
+      type: "Cha",
+    },
+    {
+      name: "Religion",
+      type: "Int",
+    },
+    {
+      name: "Sleight of Hand",
+      type: "Dex",
+    },
+    {
+      name: "Stealth",
+      type: "Dex",
+    },
+    {
+      name: "Survival",
+      type: "Wis",
+    },
+  ],
+};
+
+export default CharStatsModel;
